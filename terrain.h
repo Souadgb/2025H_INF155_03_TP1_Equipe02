6f0d463415a2452e446a31e71018070e00355d5f
cherifa2446 <cherifa.mihoub.1@ens.etsmtl.ca>
2025-03-09 18:11:38 -0400

    Update terrain.h

--- terrain.h
+++ terrain.h
@@ -8,6 +8,7 @@
 //  taille du terrain
 #define NB_LIGNES 10
 #define NB_COLONNES 10
+#define STATIOM_MAX_CARBURANT 9
 
 //  *****************************
 //  Definitions des types publics
@@ -18,6 +19,7 @@
 //  Seul ce module peut acceder aux valeurs des cases en passant par les indices des cases.
 //  Les autres modules DOIVENT utiliser les fonctions d'acces qui se trouvent dans ce module-ci.
 typedef int t_terrain[NB_LIGNES][NB_COLONNES];
+
 
 //  Quantite maximale de carburant par case de terrain
 #define STATION_MAX_CARBURANT 9
@@ -33,7 +35,7 @@ typedef int t_terrain[NB_LIGNES][NB_COLONNES];
  * @param sortie_colonne l'adresse ou deposer une colonne de sortie aleatoire
  */
 //  Ecrire le prototype de la fonction 'terrain_generer_position_sortie' ici
-
+void terrain_generer_position_sortie(int *sortie_ligne, int *sortie_colonne);
 /**
  * @brief Propose une position de depart aleatoire pour un terrain de dimensions NB_LIGNES x NB_COLONNES
  * @name terrain_generer_position_depart
@@ -44,7 +46,7 @@ typedef int t_terrain[NB_LIGNES][NB_COLONNES];
  * @note la distance Manahttan entre la position de depart et la position de sortie doit etre plus grande que 10.
  */
 //  Ecrire le prototype de la fonction 'terrain_generer_position_depart' ici
-
+void terrain_generer_position_depart(int destination_ligne, int destination_colonne, int *depart_ligne, int *depart_colonne);
 /**
  * @brief Verifie qu'une position se trouve bien dans les limites du terrain de NB_LIGNES x NB_COLONNES
  * @name terrain_contient
@@ -53,7 +55,7 @@ typedef int t_terrain[NB_LIGNES][NB_COLONNES];
  * @return true si la position se trouve a l'interieur du terrain, false sinon
  */
 //  Ecrire le prototype de la fonction 'terrain_contient' ici
-
+bool terrain_contien(int position_ligne, int position_colonne);
 /**
  * @brief Retourne la quantite de carburant qui se trouve sur une case donnee du terrain
  * @name terrain_get_carburant
@@ -63,7 +65,7 @@ typedef int t_terrain[NB_LIGNES][NB_COLONNES];
  * @return la quantite de carburant presente a cette position
  */
 //  Ecrire le prototype de la fonction 'terrain_get_carburant' ici
-
+int terrain_get_carburant(t_terrain terrain, int position_ligne, int position_colonne);
 /**
  * @brief Initialise la quantite de carburant sur une case du terrain
  * @name terrain_set_carburant
@@ -73,14 +75,14 @@ typedef int t_terrain[NB_LIGNES][NB_COLONNES];
  * @param carburant le carburant qui se trouvera dans la case
  */
 //  Ecrire le prototype de la fonction 'terrain_set_carburant' ici
-
+void terrain_set_carburant(t_terrain terrain, int position_ligne, int position_colonne, int carburant);
 /**
  * @brief Initialise toutes les cases d'un terrain a 0 (zero)
  * @name terrain_init
  * @param terrain le terrain dont on veut initialiser les cases
  */
 //  Ecrire le prototype de la fonction 'terrain_init' ici
-
+void terrain_init(t_terrain terrain);
 /**
  * @brief Cree aleatoirement les stations de gas sur le terrain. La quantite_globale des quantites de gas des stations est passee en parametre.
  * @name terrain_creer_stations_carburant
@@ -88,7 +90,7 @@ typedef int t_terrain[NB_LIGNES][NB_COLONNES];
  * @param quantite_globale la quantite globale de carburant a allouer sur l'ensemble des stations
  */
 //  Ecrire le prototype de la fonction 'terrain_creer_stations_carburant' ici
-
+void terrain_creer_stations_carburant (t_terrain terrain, int quantite_globale);
 /**
  * @brief Affiche la quantite de gas pour toutes les cases d'un terrain.
  * De plus, presente la case courante et la case de destination avec des couleurs differentes.
@@ -99,5 +101,5 @@ typedef int t_terrain[NB_LIGNES][NB_COLONNES];
  * @param destination_colonne la ligne de la position de la case de destination
  */
 //  Ecrire le prototype de la fonction 'terrain_afficher' ici
-
+void terrain_afficher(t_terrain terrain, int courante_ligne, int courante_colonne, int destination_ligne, int destination_colonne);
 #endif //CODE_TERRAIN_H
